027972c1b73a77a0f29d5e858e902f93dc898f73
Rodolfo <fororus12@gmail.com>
2026-06-17 21:51:37 -0400

    Whatsapp button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css"; 
 import { NavBar } from "@/components/NavBar"; 
 import { Footer } from "@/components/Footer";
+import { WhatsAppButton } from "@/components/WhatsAppButton";
 
 export const metadata: Metadata = {
   title: "Santorini Ships Solutions | Premium Maritime Excellence",
@@ -34,6 +35,8 @@ export default function RootLayout({
         </main>
 
         <Footer/>
+
+        <WhatsAppButton/>
 
       </body>
     </html>
